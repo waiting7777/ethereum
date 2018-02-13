98e69a64b8d0bbfb7430f7d3ed33b268d2fac2d6
waiting7777 <waiting0819@msn.com>
2018-02-13 18:21:16 +0800

    init inbox

---
+++ inbox/compile.js
@@ -0,0 +1,8 @@
+const path = require('path')
+const fs = require('fs')
+const solc = require('solc')
+
+const inboxPath = path.resolve(__dirname, 'contract', 'Inbox.sol')
+const source = fs.readFileSync(inboxPath, 'utf8')
+
+console.log(solc.compile(source, 1))
