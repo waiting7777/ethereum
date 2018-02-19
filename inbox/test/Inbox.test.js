33855acc3ec3780c328859742e98753bbbddde84
Waiting7777 <waiting0819@msn.com>
2018-02-20 04:19:37 +0800

    refactor to promise

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -3,18 +3,18 @@ const ganache = require('ganache-cli')
 const Web3 = require('web3')
 const web3 = new Web3(ganache.provider())
 
-beforeEach(() => {
+let accounts
+
+beforeEach(async () => {
     // Get a list od all account
-    web3.eth.getAccounts()
-        .then(fetchedAccounts => {
-            console.log(fetchedAccounts)
-        })
+    accounts = await web3.eth.getAccounts()
+
     // Use one of those accounts to deploy the contract
 
 })
 
 describe('Inbox', () => {
     it('deploys a contract', () => {
-        
+        console.log(accounts)
     })
 })
