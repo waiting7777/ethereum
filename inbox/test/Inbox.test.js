134a02373417cdd0db3b84b6f7c8ec9b7333334d
Waiting7777 <waiting0819@msn.com>
2018-02-20 04:17:14 +0800

    add account test

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -3,29 +3,18 @@ const ganache = require('ganache-cli')
 const Web3 = require('web3')
 const web3 = new Web3(ganache.provider())
 
-class Car {
-    park() {
-        return 'stopped'
-    }
-
-    drive() {
-        return 'vroom'
-    }
-}
-
-let car
-
 beforeEach(() => {
-    car = new Car()
+    // Get a list od all account
+    web3.eth.getAccounts()
+        .then(fetchedAccounts => {
+            console.log(fetchedAccounts)
+        })
+    // Use one of those accounts to deploy the contract
+
 })
 
-describe('Car', () => {
-
-    it('can park', () => {
-        assert.equal(car.park(), 'stopped')
-    })
-
-    it('can drive', () => {
-        assert.equal(car.drive(), 'vroom')
+describe('Inbox', () => {
+    it('deploys a contract', () => {
+        
     })
 })
